632e3a75835aa8a02c585c1b840ae90250a99ff9
Trimpta <68428753+trimpta@users.noreply.github.com>
2025-10-17 19:40:50 +0530

    remove blank file, add example config.

---
+++ config-example.js
@@ -0,0 +1,28 @@
+let task_group_id = 'id@g.us';
+exports.task_group = task_group_id;
+
+let users = ["p1", "p2", "p3", "p4", "p5", "p6", "p6"];
+let nums = ["n1", "n2", "n3", "n3", "n4", "n5", "n6"]; //nx = "xxxxxxxxxx" just the digits of phone no. without +country code or @c.us
+
+nums = nums.map((num) => {
+    return "91" + num; //my country code for convenience
+});
+
+
+let tasks = [
+    { day: "sunday", person: "p1" },
+    { day: "monday", person: "p3" },
+    { day: "tuesday", person: "p2" },
+    { day: "wednesday", person: "p4" },
+    { day: "friday", person: "p2" },
+    { day: "friday", person: "p3" },
+    { day: "saturday", person: "p6" }
+];
+tasks = tasks.map(
+    (e) => {
+        e.num = nums[users.indexOf(e.person)];
+        return e;
+    }
+);
+
+exports.tasks = tasks;
